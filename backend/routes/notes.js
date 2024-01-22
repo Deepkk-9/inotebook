ca16250e944dbc2ebb521140dd1c09d68be50adf
Deep <kawaledeep9@gmail.com>
2024-01-22 17:12:14 +0530

    Added delete route which delete a task from db

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,93 @@ router.post("/addnote", fetchuser,
 
     })
 
+
+
+router.put("/updatenote/:id", fetchuser,
+    [
+        body('title', "Enter a valid title").isLength({ min: 3 }),
+        body('description', "Description must be atleast 5 chracters").isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        try {
+
+            // Returns error when the validation fails of express-validator
+            const error = validationResult(req);
+
+            if (!error.isEmpty()) {
+                return res.status(400).json({ errors: error.array() })
+            }
+
+
+
+            // Checks if the ID of the note exist or not 
+            let note = await Notes.findById(req.params.id)
+
+            if (!note) {
+                return res.status(404).send("Not Found")
+            }
+
+            // If ID of note exist then checks if the note is of the same user
+            if (note.user.toString() !== req.id) {
+                return res.status(401).send("Not Allowed")
+            }
+
+            const { title, description, tag } = req.body;
+
+            let newNote = {}
+
+            if (title) {
+                newNote.title = title;
+            }
+            if (description) {
+                newNote.description = description;
+            }
+            if (tag) {
+                newNote.tag = tag;
+            }
+
+            note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+
+            res.send(note)
+
+        }
+        catch (err) {
+            res.status(500).send("Internal Server Error")
+        }
+
+    })
+
+
+
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+    try {
+
+        // Returns error when the validation fails of express-validator
+        const error = validationResult(req);
+
+        if (!error.isEmpty()) {
+            return res.status(400).json({ errors: error.array() })
+        }
+
+        // Checks if the ID of the note exist or not 
+        let note = await Notes.findById(req.params.id)
+
+        if (!note) {
+            return res.status(404).send("Not Found")
+        }
+
+        // If ID of note exist then checks if the note is of the same user
+        if (note.user.toString() !== req.id) {
+            return res.status(401).send("Not Allowed")
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id)
+
+        res.json({ "Success": "Note has been deleted", note: note })
+
+    } catch (err) {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
 module.exports = router
