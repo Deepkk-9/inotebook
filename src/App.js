9112c5f25eb9e1336b8b3601c01314d5c9b06fc5
Deep <kawaledeep9@gmail.com>
2024-01-25 19:13:38 +0530

    Added routing to frontend using reac-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
